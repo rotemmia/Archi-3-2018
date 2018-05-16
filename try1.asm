88bc2405bd88e1911a70e3e7b944c59df82d9a09
rotemmia <rotemmia@post.bgu.ac.il>
2018-05-16 17:14:40 +0300

    commit & push

--- try1.asm
+++ try1.asm
@@ -1,39 +1,34 @@
+neg_start_of_memory start_of_memory +
 index start_of_memory +
 A_pointer index +
 temp1 temp1 +
 temp1 A_pointer +
-init_a+1 init_a+1 +
 init_a+1 temp1 +
 temp2 temp2 +
 init_a: temp2 0 +
-temp2 start_of_memory +
 A temp2 +
 index one +
 B_pointer index +
 temp1 temp1 +
 temp1 B_pointer +
-init_b+1 init_b+1 +
 init_b+1 temp1 +
 temp2 temp2 +
 init_b: temp2 0 +
-temp2 start_of_memory +
 B temp2 +
 index one +
 C_pointer index +
 temp1 temp1 +
 temp1 C_pointer +
-init_c+1 init_c+1 +
 init_c+1 temp1 +
 temp2 temp2 +
 init_c: temp2 0 +
-temp2 start_of_memory +
 C temp2 +
 ; Check A == 0 && B == 0 && C == 0
 ; if so exit program.
   ; if A -1 < 0
 check_zero: A one check_zero_2
   ; A-1 > 0 continue loop.
-  A neg_one continue_loop
+  A neg_one +
   ; jmp continue_loop
   neg_one zero continue_loop
   ; Add 1 back if still negative then we keep going
@@ -42,7 +37,7 @@ check_zero_2:  A neg_one continue_loop
   ; if b -1 < 0
 check_zero_3:  B one check_zero_4
   ; B-1 > 0 continue loop.
-  B neg_one continue_loop
+  B neg_one +
   ; jmp continue_loop
   neg_one zero continue_loop
   ; Add 1 back if still negative then we keep going
@@ -51,14 +46,18 @@ check_zero_4: B neg_one continue_loop
   ; if C -1 < 0
 check_zero_5:  C one check_zero_6
   ; C-1 > 0 continue loop.
-  C neg_one continue_loop
+  C neg_one +
   ; jmp continue_loop
   neg_one zero continue_loop
   ; Add 1 back if still negative then we keep going
 check_zero_6:  C neg_one continue_loop
   ; A B C all equal 0. Exit.
   neg_one zero exit
-continue_loop:  A B loop_result_neg
+continue_loop:
+  A neg_start_of_memory +
+  B neg_start_of_memory +
+  C neg_start_of_memory +
+  A B loop_result_neg
   index one re_loop
   ; jmp to new index.
 loop_result_neg: index index +
@@ -69,10 +68,10 @@ re_loop: A_pointer A_pointer +
   temp1 A_pointer +
   assign_a+1 assign_a+1 +
   assign_a+1 temp1 +
-  temp2 temp2 +
-  assign_a: temp2 0 +
-  temp2 start_of_memory +
-  A temp2 +
+  temp1 temp1 +
+  assign_a: temp1 0 +
+  A A +
+  A temp1 +
   index one +
   B_pointer B_pointer +
   B_pointer index +
@@ -80,9 +79,11 @@ re_loop: A_pointer A_pointer +
   temp1 B_pointer +
   assign_b+1 assign_b+1 +
   assign_b+1 temp1 +
+  temp1 temp1 +
+  assign_b: temp1 0 +
+  B B +
   temp2 temp2 +
-  assign_b: temp2 0 +
-  temp2 start_of_memory +
+  temp2 temp1 +
   B temp2 +
   index one +
   C_pointer C_pointer +
@@ -91,34 +92,26 @@ re_loop: A_pointer A_pointer +
   temp1 C_pointer +
   assign_c+1 assign_c+1 +
   assign_c+1 temp1 +
+  temp1 temp1 +
+  assign_c: temp1 0 +
+  C C +
   temp2 temp2 +
-  assign_c: temp2 0 +
-  temp2 start_of_memory +
-  C temp2 +
-
-  ; jump to start of loop (check ABC != 0)
+  temp2 temp1 +
+  C temp2 +  ; jump to start of loop (check ABC != 0)
   neg_one zero check_zero
 exit: 0 0 0
-three: 3
-zero: 0
 neg_one: -1
-one: 1
-index: 0
-; TODO
 A: 0
-A_pointer: 0
-Areturn: neg_one zero return_address
 B: 0
-B_pointer: 0
-Breturn: neg_one zero return_address
 C: 0
+one: 1
+zero: 0
+A_pointer: 0
+B_pointer: 0
 C_pointer: 0
-Creturn: neg_one zero return_address
+index: 0
 temp1: 0
 temp2: 0
-temp3: 0
-temp4: 0
-temp5: 0
 return_address: 0
-neg_start_of_memory: +
+neg_start_of_memory: 0
 start_of_memory: +
